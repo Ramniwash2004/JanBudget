b16645f8de53d12edb65d272d93a177f3794bf90
Ramniwash2004 <kramniwash68@gmail.com>
2025-11-05 13:34:20 +0530

    Add proposal data,like,comment,wardBudgetOverview,announcements

---
+++ Client/src/components/data/wardBudgetOverviewData.ts
@@ -0,0 +1,645 @@
+export const wardData=[
+  {
+    "id": 1,
+    "name": "Ward 1 - Veer Savarkar Nagar Ward",
+    "totalBudget": 3045000,
+    "allocated": 2131500,
+    "ongoing": 3,
+    "completed": 4
+  },
+  {
+    "id": 2,
+    "name": "Ward 2 - Pt. Javaharlal Nehru Ward",
+    "totalBudget": 3090000,
+    "allocated": 2163000,
+    "ongoing": 4,
+    "completed": 5
+  },
+  {
+    "id": 3,
+    "name": "Ward 3 - Sant Kabir Das Ward",
+    "totalBudget": 3135000,
+    "allocated": 2194500,
+    "ongoing": 5,
+    "completed": 6
+  },
+  {
+    "id": 4,
+    "name": "Ward 4 - Yatiyatan Lal Ward",
+    "totalBudget": 3180000,
+    "allocated": 2226000,
+    "ongoing": 6,
+    "completed": 7
+  },
+  {
+    "id": 5,
+    "name": "Ward 5 - Banjari Mata Mandir Ward",
+    "totalBudget": 3225000,
+    "allocated": 2257500,
+    "ongoing": 2,
+    "completed": 8
+  },
+  {
+    "id": 6,
+    "name": "Ward 6 - Veerangani Avanti Bai Ward",
+    "totalBudget": 3270000,
+    "allocated": 2289000,
+    "ongoing": 3,
+    "completed": 3
+  },
+  {
+    "id": 7,
+    "name": "Ward 7 - Kushabhav Thakre",
+    "totalBudget": 3315000,
+    "allocated": 2320500,
+    "ongoing": 4,
+    "completed": 4
+  },
+  {
+    "id": 8,
+    "name": "Ward 8 - Pt. Motilal Nehru Ward",
+    "totalBudget": 3360000,
+    "allocated": 2352000,
+    "ongoing": 5,
+    "completed": 5
+  },
+  {
+    "id": 9,
+    "name": "Ward 9 - Doc. Bhimrav Ambedkar Ward",
+    "totalBudget": 3405000,
+    "allocated": 2383500,
+    "ongoing": 6,
+    "completed": 6
+  },
+  {
+    "id": 10,
+    "name": "Ward 10 - Rani Laxmi Bai Ward",
+    "totalBudget": 3450000,
+    "allocated": 2415000,
+    "ongoing": 2,
+    "completed": 7
+  },
+  {
+    "id": 11,
+    "name": "Ward 11 - Kalimata Ward",
+    "totalBudget": 3495000,
+    "allocated": 2446500,
+    "ongoing": 3,
+    "completed": 8
+  },
+  {
+    "id": 12,
+    "name": "Ward 12 - Mahatma Gandhi Ward",
+    "totalBudget": 3540000,
+    "allocated": 2478000,
+    "ongoing": 4,
+    "completed": 3
+  },
+  {
+    "id": 13,
+    "name": "Ward 13 - Rajiv Gandi Ward",
+    "totalBudget": 3585000,
+    "allocated": 2509500,
+    "ongoing": 5,
+    "completed": 4
+  },
+  {
+    "id": 14,
+    "name": "Ward 14 - Raman Mandir Ward",
+    "totalBudget": 3630000,
+    "allocated": 2541000,
+    "ongoing": 6,
+    "completed": 5
+  },
+  {
+    "id": 15,
+    "name": "Ward 15 - Kanhiya Lal Banjari Ward",
+    "totalBudget": 3675000,
+    "allocated": 2572500,
+    "ongoing": 2,
+    "completed": 6
+  },
+  {
+    "id": 16,
+    "name": "Ward 16 - Veer Shivaji Ward",
+    "totalBudget": 3720000,
+    "allocated": 2604000,
+    "ongoing": 3,
+    "completed": 7
+  },
+  {
+    "id": 17,
+    "name": "Ward 17 - Thakkar Bapa Ward",
+    "totalBudget": 3765000,
+    "allocated": 2635500,
+    "ongoing": 4,
+    "completed": 8
+  },
+  {
+    "id": 18,
+    "name": "Ward 18 - Bal Gangadhar Tilak Nagar",
+    "totalBudget": 3810000,
+    "allocated": 2667000,
+    "ongoing": 5,
+    "completed": 3
+  },
+  {
+    "id": 19,
+    "name": "Ward 19 - DR. A.P.J. ABDUL KALAM WARD",
+    "totalBudget": 3855000,
+    "allocated": 2698500,
+    "ongoing": 6,
+    "completed": 4
+  },
+  {
+    "id": 20,
+    "name": "Ward 20 - Ram Krishna Paramhans Ward",
+    "totalBudget": 3900000,
+    "allocated": 2730000,
+    "ongoing": 2,
+    "completed": 5
+  },
+  {
+    "id": 21,
+    "name": "Ward 21 - Shaheed Bhagat Singh Ward",
+    "totalBudget": 3945000,
+    "allocated": 2761500,
+    "ongoing": 3,
+    "completed": 6
+  },
+  {
+    "id": 22,
+    "name": "Ward 22 - Pandit Ishwaricharan Shukla Ward",
+    "totalBudget": 3990000,
+    "allocated": 2793000,
+    "ongoing": 4,
+    "completed": 7
+  },
+  {
+    "id": 23,
+    "name": "Ward 23 - Manmohan Singh Bakshi Ward",
+    "totalBudget": 4035000,
+    "allocated": 2824500,
+    "ongoing": 5,
+    "completed": 8
+  },
+  {
+    "id": 24,
+    "name": "Ward 24 - Vallab Bhai Patel Ward",
+    "totalBudget": 4080000,
+    "allocated": 2856000,
+    "ongoing": 6,
+    "completed": 3
+  },
+  {
+    "id": 25,
+    "name": "Ward 25 - Sant Ram Das Ward",
+    "totalBudget": 4125000,
+    "allocated": 2887500,
+    "ongoing": 2,
+    "completed": 4
+  },
+  {
+    "id": 26,
+    "name": "Ward 26 - Danveer Bhabhasaha Ward",
+    "totalBudget": 4170000,
+    "allocated": 2919000,
+    "ongoing": 3,
+    "completed": 5
+  },
+  {
+    "id": 27,
+    "name": "Ward 27 - Indra Gandhi Ward",
+    "totalBudget": 4215000,
+    "allocated": 2950500,
+    "ongoing": 4,
+    "completed": 6
+  },
+  {
+    "id": 28,
+    "name": "Ward 28 - Shaheed Hemu Kalyani Ward",
+    "totalBudget": 4260000,
+    "allocated": 2982000,
+    "ongoing": 5,
+    "completed": 7
+  },
+  {
+    "id": 29,
+    "name": "Ward 29 - Guru Govind Singh Ward",
+    "totalBudget": 4305000,
+    "allocated": 3013500,
+    "ongoing": 6,
+    "completed": 8
+  },
+  {
+    "id": 30,
+    "name": "Ward 30 - Shankar Nagar Ward",
+    "totalBudget": 4350000,
+    "allocated": 3045000,
+    "ongoing": 2,
+    "completed": 3
+  },
+  {
+    "id": 31,
+    "name": "Ward 31 - Neta Shubhaschandra Bose Ward",
+    "totalBudget": 4395000,
+    "allocated": 3076500,
+    "ongoing": 3,
+    "completed": 4
+  },
+  {
+    "id": 32,
+    "name": "Ward 32 - Maharishi Valmiki Ward",
+    "totalBudget": 4440000,
+    "allocated": 3108000,
+    "ongoing": 4,
+    "completed": 5
+  },
+  {
+    "id": 33,
+    "name": "Ward 33 - Veernarayan Singh Ward",
+    "totalBudget": 4485000,
+    "allocated": 3139500,
+    "ongoing": 5,
+    "completed": 6
+  },
+  {
+    "id": 34,
+    "name": "Ward 34 - Lal Bhadur Shastri Ward",
+    "totalBudget": 4530000,
+    "allocated": 3171000,
+    "ongoing": 6,
+    "completed": 7
+  },
+  {
+    "id": 35,
+    "name": "Ward 35 - Pandit Ravishankar Shukla Ward",
+    "totalBudget": 4575000,
+    "allocated": 3202500,
+    "ongoing": 2,
+    "completed": 8
+  },
+  {
+    "id": 36,
+    "name": "Ward 36 - Havaldar Abdul Hamid Ward",
+    "totalBudget": 4620000,
+    "allocated": 3234000,
+    "ongoing": 3,
+    "completed": 3
+  },
+  {
+    "id": 37,
+    "name": "Ward 37 - Tatyapara Ward Shaheed",
+    "totalBudget": 4665000,
+    "allocated": 3265500,
+    "ongoing": 4,
+    "completed": 4
+  },
+  {
+    "id": 38,
+    "name": "Ward 38 - Chudamani Nayak Ward",
+    "totalBudget": 4710000,
+    "allocated": 3297000,
+    "ongoing": 5,
+    "completed": 5
+  },
+  {
+    "id": 39,
+    "name": "Ward 39 - Swami Atmanand Ward",
+    "totalBudget": 4755000,
+    "allocated": 3328500,
+    "ongoing": 6,
+    "completed": 6
+  },
+  {
+    "id": 40,
+    "name": "Ward 40 - Thakur Pyare Lal Ward",
+    "totalBudget": 4800000,
+    "allocated": 3360000,
+    "ongoing": 2,
+    "completed": 7
+  },
+  {
+    "id": 41,
+    "name": "Ward 41 - Pt. Dindayal Uppadhye Ward",
+    "totalBudget": 4845000,
+    "allocated": 3391500,
+    "ongoing": 3,
+    "completed": 8
+  },
+  {
+    "id": 42,
+    "name": "Ward 42 - Pt. Sundar Lal Sharma Ward",
+    "totalBudget": 4890000,
+    "allocated": 3423000,
+    "ongoing": 4,
+    "completed": 3
+  },
+  {
+    "id": 43,
+    "name": "Ward 43 - Mahant Laxminarayn Das Ward",
+    "totalBudget": 4935000,
+    "allocated": 3454500,
+    "ongoing": 5,
+    "completed": 4
+  },
+  {
+    "id": 44,
+    "name": "Ward 44 - Bhramanpara Ward",
+    "totalBudget": 4980000,
+    "allocated": 3486000,
+    "ongoing": 6,
+    "completed": 5
+  },
+  {
+    "id": 45,
+    "name": "Ward 45 - Swami Vivakanand Ward",
+    "totalBudget": 5025000,
+    "allocated": 3517500,
+    "ongoing": 2,
+    "completed": 6
+  },
+  {
+    "id": 46,
+    "name": "Ward 46 - Mailana Abdul Rauf Ward",
+    "totalBudget": 5070000,
+    "allocated": 3549000,
+    "ongoing": 3,
+    "completed": 7
+  },
+  {
+    "id": 47,
+    "name": "Ward 47 - Civil Lines Ward",
+    "totalBudget": 5115000,
+    "allocated": 3580500,
+    "ongoing": 4,
+    "completed": 8
+  },
+  {
+    "id": 48,
+    "name": "Ward 48 - Mother Terisa Ward",
+    "totalBudget": 5160000,
+    "allocated": 3612000,
+    "ongoing": 5,
+    "completed": 3
+  },
+  {
+    "id": 49,
+    "name": "Ward 49 - Guru Ghasidas Ward",
+    "totalBudget": 5205000,
+    "allocated": 3643500,
+    "ongoing": 6,
+    "completed": 4
+  },
+  {
+    "id": 50,
+    "name": "Ward 50 - Rani Durgavati Ward",
+    "totalBudget": 5250000,
+    "allocated": 3674999,
+    "ongoing": 2,
+    "completed": 5
+  },
+  {
+    "id": 51,
+    "name": "Ward 51 - Doc. Rajendra Prasad Ward",
+    "totalBudget": 5295000,
+    "allocated": 3706499,
+    "ongoing": 3,
+    "completed": 6
+  },
+  {
+    "id": 52,
+    "name": "Ward 52 - PT. VIDYACHARAN SHUKLA WARD",
+    "totalBudget": 5340000,
+    "allocated": 3737999,
+    "ongoing": 4,
+    "completed": 7
+  },
+  {
+    "id": 53,
+    "name": "Ward 53 - Babu Jagjivan Ram Ward",
+    "totalBudget": 5385000,
+    "allocated": 3769499,
+    "ongoing": 5,
+    "completed": 8
+  },
+  {
+    "id": 54,
+    "name": "Ward 54 - KAMRED SUDHIR MUKHARJEE WARD",
+    "totalBudget": 5430000,
+    "allocated": 3800999,
+    "ongoing": 6,
+    "completed": 3
+  },
+  {
+    "id": 55,
+    "name": "Ward 55 - Ravindra Nath Taigore Ward",
+    "totalBudget": 5475000,
+    "allocated": 3832499,
+    "ongoing": 2,
+    "completed": 4
+  },
+  {
+    "id": 56,
+    "name": "Ward 56 - Arvind Dikshit Ward",
+    "totalBudget": 5520000,
+    "allocated": 3863999,
+    "ongoing": 3,
+    "completed": 5
+  },
+  {
+    "id": 57,
+    "name": "Ward 57 - Pt. Bhagwati Charan Shukla Ward",
+    "totalBudget": 5565000,
+    "allocated": 3895499,
+    "ongoing": 4,
+    "completed": 6
+  },
+  {
+    "id": 58,
+    "name": "Ward 58 - Shaheed Pankaj Vikaram Ward",
+    "totalBudget": 5610000,
+    "allocated": 3926999,
+    "ongoing": 5,
+    "completed": 7
+  },
+  {
+    "id": 59,
+    "name": "Ward 59 - Mureshwar Rao Gandre Ward",
+    "totalBudget": 5655000,
+    "allocated": 3958499,
+    "ongoing": 6,
+    "completed": 8
+  },
+  {
+    "id": 60,
+    "name": "Ward 60 - Chandrashekhar Aazad Ward",
+    "totalBudget": 5700000,
+    "allocated": 3989999,
+    "ongoing": 2,
+    "completed": 3
+  },
+  {
+    "id": 61,
+    "name": "Ward 61 - Doc. Shyam Prasad Mukhrji",
+    "totalBudget": 5745000,
+    "allocated": 4021499,
+    "ongoing": 3,
+    "completed": 4
+  },
+  {
+    "id": 62,
+    "name": "Ward 62 - Shaheed Rajiv Pard Ward",
+    "totalBudget": 5790000,
+    "allocated": 4052999,
+    "ongoing": 4,
+    "completed": 5
+  },
+  {
+    "id": 63,
+    "name": "Ward 63 - Brigediyar Usman Ward",
+    "totalBudget": 5835000,
+    "allocated": 4084499,
+    "ongoing": 5,
+    "completed": 6
+  },
+  {
+    "id": 64,
+    "name": "Ward 64 - Doc. Vipin Bihari Sur Ward",
+    "totalBudget": 5880000,
+    "allocated": 4115999,
+    "ongoing": 6,
+    "completed": 7
+  },
+  {
+    "id": 65,
+    "name": "Ward 65 - Mahamaya Mandir Ward",
+    "totalBudget": 5925000,
+    "allocated": 4147499,
+    "ongoing": 2,
+    "completed": 8
+  },
+  {
+    "id": 66,
+    "name": "Ward 66 - Vaman Rao Lakhe Ward",
+    "totalBudget": 5970000,
+    "allocated": 4178999,
+    "ongoing": 3,
+    "completed": 3
+  },
+  {
+    "id": 67,
+    "name": "Ward 67 - BHAKTMATA KARMA WARD",
+    "totalBudget": 6015000,
+    "allocated": 4210500,
+    "ongoing": 4,
+    "completed": 4
+  },
+  {
+    "id": 68,
+    "name": "Ward 68 - Doc. Khubchand Baghel Ward",
+    "totalBudget": 6060000,
+    "allocated": 4242000,
+    "ongoing": 5,
+    "completed": 5
+  },
+  {
+    "id": 69,
+    "name": "Ward 69 - Madhav Rav Sapre Ward",
+    "totalBudget": 6105000,
+    "allocated": 4273500,
+    "ongoing": 6,
+    "completed": 6
+  },
+  {
+    "id": 70,
+    "name": "Ward 70 - Sant Ravidas Ward",
+    "totalBudget": 6150000,
+    "allocated": 4305000,
+    "ongoing": 2,
+    "completed": 7
+  }
+];
+
+export const announcements = [
+  {
+    id: 1,
+    title: "Voting Open for Green Infrastructure Projects",
+    description:
+      "Citizens are encouraged to vote for eco-friendly development proposals across all wards.",
+    date: "3 days ago",
+    type: "voting",
+  },
+  {
+    id: 2,
+    title: "Digital Library Inaugurated in Ward 4",
+    description:
+      "A modern library with free internet access and e-learning facilities has been opened for the public.",
+    date: "5 days ago",
+    type: "completed",
+  },
+  {
+    id: 3,
+    title: "Proposal Deadline Extended to December 20",
+    description:
+      "Due to high participation, the submission deadline for new proposals has been extended.",
+    date: "1 week ago",
+    type: "deadline",
+  },
+  {
+    id: 4,
+    title: "Voting Begins for Smart Streetlight Initiative",
+    description:
+      "Residents can now vote on the implementation of solar-powered streetlights in their wards.",
+    date: "2 days ago",
+    type: "voting",
+  },
+  {
+    id: 5,
+    title: "Community Health Center Renovation Completed",
+    description:
+      "The health center in Ward 21 is now fully operational with upgraded medical equipment.",
+    date: "4 days ago",
+    type: "completed",
+  },
+  {
+    id: 6,
+    title: "Proposal Review Phase Closes Tomorrow",
+    description:
+      "Final day to review and provide feedback on shortlisted citizen proposals before voting begins.",
+    date: "1 day ago",
+    type: "deadline",
+  },
+  {
+    id: 7,
+    title: "Public WiFi Network Now Active in Ward 18",
+    description:
+      "Free high-speed internet is now available in community parks and public areas of Ward 18.",
+    date: "6 days ago",
+    type: "completed",
+  },
+  {
+    id: 8,
+    title: "Voting Live for Heritage Restoration Projects",
+    description:
+      "Participate in choosing which heritage sites should receive restoration funding this quarter.",
+    date: "Today",
+    type: "voting",
+  },
+  {
+    id: 9,
+    title: "Final Call for Q1 Budget Proposal Submissions",
+    description:
+      "Submit your development project ideas by midnight today to be considered in the Q1 cycle.",
+    date: "Today",
+    type: "deadline",
+  },
+  {
+    id: 10,
+    title: "Rainwater Harvesting Units Completed in Ward 9",
+    description:
+      "Ward 9 successfully completed installation of community rainwater collection systems.",
+    date: "3 weeks ago",
+    type: "completed",
+  },
+];
